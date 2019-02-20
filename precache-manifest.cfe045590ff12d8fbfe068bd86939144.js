9d4e680a6660b1893c959034dddb46441acbd13a
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-02-20 16:41:31 +0300

    Updates

---
+++ precache-manifest.cfe045590ff12d8fbfe068bd86939144.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "40af334f8d5f94aebe47",
+    "url": "/project_2A/static/css/main.beeeb2cc.chunk.css"
+  },
+  {
+    "revision": "40af334f8d5f94aebe47",
+    "url": "/project_2A/static/js/main.40af334f.chunk.js"
+  },
+  {
+    "revision": "6ced065c913cb01b65b0",
+    "url": "/project_2A/static/js/runtime~main.6ced065c.js"
+  },
+  {
+    "revision": "a941221767de8512cd3e",
+    "url": "/project_2A/static/js/2.a9412217.chunk.js"
+  },
+  {
+    "revision": "8d344d274340fe4274ec909c875f12a4",
+    "url": "/project_2A/static/media/pen2.8d344d27.png"
+  },
+  {
+    "revision": "79a85067e91168fdfdee3d7dc8f28f48",
+    "url": "/project_2A/static/media/add.79a85067.png"
+  },
+  {
+    "revision": "04169f1fec25887a2796e8fd346781b7",
+    "url": "/project_2A/static/media/erase.04169f1f.png"
+  },
+  {
+    "revision": "6b1ae0566b2394c9748164a977b6c412",
+    "url": "/project_2A/static/media/pin.6b1ae056.jpg"
+  },
+  {
+    "revision": "d53546e0537539ffac2a1bd995789c14",
+    "url": "/project_2A/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/project_2/precache-manifest.804d187ddb81c5f3b191d380083de90a.js"
+  "/project_2A/precache-manifest.cfe045590ff12d8fbfe068bd86939144.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/project_2/index.html", {
+workbox.routing.registerNavigationRoute("/project_2A/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
